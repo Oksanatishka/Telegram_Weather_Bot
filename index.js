083c756004a32dce3a6252ef5b5b1fef9db8742f
Oksana <oksanabibik93@gmail.com>
2019-08-28 21:51:51 -0700

    add files

---
+++ index.js
@@ -0,0 +1,88 @@
+const TelegramBot = require('node-telegram-bot-api'); // developing server side of our bot.
+const axios = require('axios'); // instead standard http package
+
+// Telegram's token
+const token = '978830341:AAHBqBa8m8YKDy_FKOUYnFkAkFI3rd6m0uk';
+
+//OpenWeatherMap API key
+const appID = '4a5a985d7379ba1e2f25c78673337717';
+
+// OpenWeatherMap endpoint for getting weather by city name
+const weatherEndpoint = city =>
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${appID}`;
+
+// URL that provides icon according to the weather
+const weatherIcon = icon => `http://openweathermap.org/img/w/${icon}.png`;
+
+// Template for weather response
+const weatherHtmlTemplate = (name, main, weather, wind, clouds) =>
+    `The weather in <b>${name}</b>:
+<b>${weather.main}</b> - ${weather.description}
+Temperature: <b>${main.temp} °C</b>
+Pressure: <b>${main.pressure} hPa</b>
+Humidity: <b>${main.humidity} %</b>
+Wind: <b>${wind.speed} meter/sec</b>
+Clouds: <b>${clouds.all} %</b>
+`;
+
+// Created instance of TelegramBot
+const bot = new TelegramBot(token, {
+    polling: true
+});
+
+// Function that gets the weather by the city name
+const getWeather = (chatId, city) => {
+    const endpoint = weatherEndpoint(city);
+
+    axios.get(endpoint).then(
+        resp => {
+            const { name, main, weather, wind, clouds } = resp.data;
+
+            bot.sendPhoto(chatId, weatherIcon(weather[0].icon));
+            bot.sendMessage(chatId, weatherHtmlTemplate(name, main, weather[0], wind, clouds), {
+                parse_mode: 'HTML'
+            });
+        },
+        error => {
+            console.log('error', error);
+            bot.sendMessage(
+                chatId,
+                `Ooops...I couldn't be able to get weather for <b>${city}</b>`,
+                {
+                    parse_mode: 'HTML'
+                }
+            );
+        }
+    );
+};
+
+// Listener (handler) for telegram's /weather event
+bot.onText(/\/weather/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const city = match.input.split(' ')[1];
+
+    if (city === undefined) {
+        bot.sendMessage(chatId, `Please provide city name`);
+        return;
+    }
+    getWeather(chatId, city);
+});
+
+// Listener (handler) for telegram's /start event
+// This event happened when you start the conversation with both by the very first time
+// Provide the list of available commands
+bot.onText(/\/start/, msg => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId,
+        `Welcome at <b>MyTestWeatherInfoBot</b>, thank you for using my service
+    
+Available commands:
+
+/weather <b>city</b> - shows weather for selected <b>city</b>
+  `,
+        {
+            parse_mode: 'HTML'
+        }
+    );
+});
